0f1c620a7677814103b594519c7a8595ebbf032c
Filipe Gonçalves <filipe@codinghighway.com>
2015-04-19 17:41:31 +0100

    Removed old docs.

--- config/error/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["ConfigErrorKind","Possible error kinds"]],"fn":[["from_io_err","Converts an I/O Error into a `ConfigError`"],["from_parse_err","Converts a `ParseError` into a `ConfigError`"]],"struct":[["ConfigError","A generic configuration error type"]]});
